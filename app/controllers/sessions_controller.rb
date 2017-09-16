db694e0a08448f300955e58b55a28e2c2508286e
Matt <matt@Matts-MacBook-Air.local>
2017-09-16 11:53:36 +0200

    more login bugfix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,14 @@
 class SessionsController < ApplicationController
   def login
     @user = User.find_by(email: params[:email])
-    if @user.password == params[:password]
-      response = @user.id
-    else
+    if @user == nil
       response = 0
+    else
+      if @user.password == params[:password]
+        response = @user.id
+      else
+        response = 0
+      end
     end
     render json: response
   end
