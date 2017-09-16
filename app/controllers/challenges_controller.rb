1bd849445404c9f8802892565d5ef5c769147776
Matt <matt@Matts-MacBook-Air.local>
2017-09-16 11:18:01 +0200

    bugfix

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -27,7 +27,6 @@ class ChallengesController < ApplicationController
     @challenge.description = params[:description]
     @challenge.price = params[:price]
     @challenge.challenger_id = params[:user_id]
-    render json: @challenge
     @challenge.save!
 
     render json: 1
