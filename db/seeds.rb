01345458aadad58efba1873491e671d5a75ed520
Matt <matt@Matts-MacBook-Air.local>
2017-09-17 06:53:57 +0200

    updates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,15 @@
 
 
 10.times do
-  User.create(email: Faker::Internet.email, username: Faker::Name.name, password: 123, balance: rand(100))
+  User.create(email: Faker::Internet.email, username: Faker::Name.first_name, password: 123)
 end
 
 
 20.times do
-  Challenge.create(title: Faker::Job.title, description: Faker::ChuckNorris.fact, price: rand(100..500), challenger_id: rand(10))
+  Challenge.create(title: Faker::RickAndMorty.character, description: Faker::RickAndMorty.quote, price: rand(100..500), challenger_id: rand(10))
 end
 
 
-200.times do
-  Payment.create(amount: rand(1..50), challenge_id: rand(20), payer_id: rand(10))
+60.times do
+  Payment.create(amount: rand(20..50), challenge_id: rand(20), payer_id: rand(10))
 end
