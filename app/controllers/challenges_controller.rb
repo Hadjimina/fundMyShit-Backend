c17b0afae41657642122ca42ec34b91d77006fdf
Matt <matt@Matts-MacBook-Air.local>
2017-09-17 05:29:39 +0200

    m

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -10,7 +10,7 @@ class ChallengesController < ApplicationController
     end
 
 
-    render json: @challenges.reverse
+    render json: @challenges
   end
 
   def show
@@ -58,7 +58,7 @@ class ChallengesController < ApplicationController
       # @challenges =  @challenges.sort_by { |a| a.complete ? 0 : 1 }
 
 
-      render json: @challenges.reverse
+      render json: @challenges
 
   end
 
